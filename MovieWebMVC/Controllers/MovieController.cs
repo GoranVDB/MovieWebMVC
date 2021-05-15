85ab86c958ae2868857006f8e5aa2d38ca1722de
Goran Vandenbossche <Goran.Vandenbossche@realdolmen.com>
2021-05-15 19:01:00 +0200

    zonder viewmodel

---
+++ MovieWebMVC/Controllers/MovieController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieWebMVC.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieWebMVC.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult Detail()
+        {
+            var movie = new Movie()
+            {
+                Title = "The Lionking",
+                Description = "The best movie ever",
+                Genre = "Animatie film",
+                Price = (decimal)4.99,
+                Rating = 5,
+                Director = "Jon Favreau",
+                ReleaseDate = new DateTime(1994, 5, 4)
+            };
+            return View(movie);
+        }
+    }
+}
---
+++ MovieWebMVC/Domain/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieWebMVC.Domain
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public string Director { get; set; }
+        public decimal Price { get; set; }
+        public decimal Rating { get; set; }
+    }
+}
